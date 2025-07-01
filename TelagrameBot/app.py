90dacdad7713c784ee5ae5b472240bff58b5a3a5
jeyachandranj <j.jeyachandran072@gmail.com>
2025-07-01 15:44:14 +0530

    add

--- TelagrameBot/app.py
+++ TelagrameBot/app.py
@@ -481,4 +481,5 @@ def home():
 
 if __name__ == '__main__':
     # For development
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
